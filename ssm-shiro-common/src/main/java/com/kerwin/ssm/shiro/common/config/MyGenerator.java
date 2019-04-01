79d1d3bb7736e331170a8e6da8a67e2fec07306d
kerwin-wang <wdzdh09@163.com>
2019-04-01 15:02:16 +0800

    add mybatis generator database table comment to entity comment

---
+++ ssm-shiro-common/src/main/java/com/kerwin/ssm/shiro/common/config/MyGenerator.java
@@ -0,0 +1,76 @@
+package com.kerwin.ssm.shiro.common.config;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.InnerClass;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * @ClassName: MyGenerator
+ * @Description:
+ * @version: v1.0.0
+ * @Author: d.wang
+ * @Date: 2019-04-01 14:55
+ */
+public class MyGenerator extends DefaultCommentGenerator
+{
+    private Properties properties;
+    private Properties systemPro;
+    private boolean suppressDate;
+    private boolean suppressAllComments;
+    private String currentDateStr;
+
+    public MyGenerator() {
+        super();
+        properties = new Properties();
+        systemPro = System.getProperties();
+        suppressDate = false;
+        suppressAllComments = false;
+        currentDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+    }
+
+
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable,
+            IntrospectedColumn introspectedColumn) {
+        if (suppressAllComments) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        field.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(introspectedColumn.getRemarks());
+        field.addJavaDocLine(sb.toString().replace("\n", " "));
+        field.addJavaDocLine(" */");
+    }
+
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+
+    }
+
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+
+    }
+
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable,
+            IntrospectedColumn introspectedColumn) {
+
+    }
+
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable,
+            IntrospectedColumn introspectedColumn) {
+
+    }
+
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+
+    }
+
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+    }
+}
